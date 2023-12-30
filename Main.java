db3d3a2da819616d79ceca00acde64aac7c39ccc
Name Surname <20221500282@newuzbekistanuniversity.uz>
2023-12-30 15:40:58 +0500

    Getters and Setter usage

---
+++ Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("StartUp:");
+        User Startup = new User();
+        Startup.set_name("UFeel");
+        Startup.membership = "Fahriddin";
+        User Startup2 = new User();
+        Startup2.set_name("Eplant");
+        Startup2.membership = "Fyodor";
+        User Startup3 = new User();
+        Startup3.set_name("XFire");
+        Startup3.membership = "Akmal";
+        User Startup4 = new User();
+        Startup4.set_name("OyTickets");
+        Startup4.membership = "Nurislom";
+        User Startup5 = new User();
+        Startup5.set_name("MedBook");
+        Startup5.membership = "Karimakhon";
+        User Startup6 = new User();
+        Startup6.set_name("Flymart");
+        Startup6.membership = "Alijon";
+        User Startup7 = new User();
+        Startup7.set_name("Speaklish");
+        Startup7.membership = "Bobirjon";
+        User Startup8 = new User();
+        Startup8.set_name("Fashionista");
+        Startup8.membership = "Nafisa";
+        User Startup9 = new User();
+        Startup9.set_name("Steppeup");
+        Startup9.membership = "Alisher";
+        User Startup10 = new User();
+        Startup10.set_name("Biofuel");
+        Startup10.membership = "Maxmud";
+        User Startup11 = new User();
+        Startup11.set_name("Rent Market");
+        Startup11.membership = "Akmaljon";
+        User Startup12 = new User();
+        Startup12.set_name("Engline");
+        Startup12.membership = "Shakhlo";
+        User Startup13 = new User();
+        Startup13.set_name("Activo");
+        Startup13.membership = "Gulasal";
+        User Startup14 = new User();
+        Startup14.set_name("Rastik");
+        Startup14.membership = "Shuxrat";
+
+        System.out.println(Startup.get_name());
+        System.out.println(Startup.membership);
+    }
+}
---
+++ User.java
@@ -0,0 +1,13 @@
+public class User {
+    private String name;
+    String membership;
+    void set_name(String name){ //This is needed to give a value
+        // System.out.print("1"); can be used but it will print 1 x times(x = number of startups);
+
+        this.name = name; //_name can also be used instead of this.name
+    }
+    String get_name() { //This is needed to get the value
+        return name;
+    }
+}
+// capsulation
