6d76e1ca1dfa567d76ed00890ec48321eb7bde49
Name Surname <20221500282@newuzbekistanuniversity.uz>
2023-12-30 15:56:44 +0500

    Getters and Setter usage

--- User.java
+++ User.java
@@ -1,13 +1,19 @@
 public class User {
     private String name;
-    String membership;
+    private String membership;
     void set_name(String name){ //This is needed to give a value
         // System.out.print("1"); can be used but it will print 1 x times(x = number of startups);
 
         this.name = name; //_name can also be used instead of this.name
     }
+
     String get_name() { //This is needed to get the value
         return name;
     }
+    void set_membership(String membership) {
+        this.membership=membership;
+    }
+    String get_membership() {
+        return membership;
+    }
 }
-// capsulation
